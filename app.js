6f11480242a12a229e295a4622d85f03a34e1c54
shelton Gillum <coffejs21@gmail.com>
2024-05-25 12:33:05 -0700

    variable names updated to proper variable names

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const form = document.listForm
-const chai = document.getElementById("workingList")
-const pup = document.getElementById("itemInput")
+const listOfItems = document.getElementById("workingList")
+const itemInput = document.getElementById("itemInput")
 
 /**the button is in the form so 
  * all you have to do is create a submit event
- */
+*/
 
 const addItem = form.addEventListener("submit", (e)=>{
     e.preventDefault()
-    const gold = pup.value
     let itemElement = document.createElement("li")
-    itemElement.textContent = gold
-    chai.appendChild(itemElement)
-
+    const inputValue = itemInput.value
+    itemElement.textContent = inputValue
+    listOfItems.appendChild(itemElement)
 })
