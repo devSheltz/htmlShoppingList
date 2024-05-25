8e796c93bc80ac1721bf3b55d23fe594321d0251
shelton Gillum <coffejs21@gmail.com>
2024-05-25 12:38:05 -0700

    delete button created

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const form = document.listForm
 const listOfItems = document.getElementById("workingList")
 const itemInput = document.getElementById("itemInput")
+const deleteButton = document.createElement("button")
+deleteButton.textContent = "x"
 
-/**the button is in the form so 
- * all you have to do is create a submit event
-*/
 
 const addItem = form.addEventListener("submit", (e)=>{
     e.preventDefault()
     let itemElement = document.createElement("li")
     const inputValue = itemInput.value
     itemElement.textContent = inputValue
-    listOfItems.appendChild(itemElement)
+    listOfItems.append(itemElement, deleteButton)
 })
+
